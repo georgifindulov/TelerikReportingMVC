5e5b2fc50696309d3effd1fd80c15175a5c2ea7e
Georgi Findulov <george.findulov@outlook.com>
2025-02-04 16:08:17 +0200

    Added UI for refreshing the report with dynamic parameters.

--- TelerikReportingMVC/wwwroot/js/index.js
+++ TelerikReportingMVC/wwwroot/js/index.js
@@ -1,8 +1,14 @@
 ﻿$(document).ready(function () {
+    initializeReportViewer();
+
+    $('#btnRefresh').on('click', onRefreshReport);
+});
+
+function initializeReportViewer() {
     const reportViewer = $('#reportViewer');
 
     if (reportViewer) {
-        const numberOfReportItemsToTake = $('#numberOfReportItemsToTake').val();
+        const numberOfReportItemsToTake = $('#NumberOfReportItemsToTake').val();
 
         reportViewer.telerik_ReportViewer({
             serviceUrl: "api/reports/",
@@ -19,4 +25,20 @@
             sendEmail: { enabled: true }
         });
     }
-});
+}
+
+function onRefreshReport() {
+    const numberOfReportItemsToTake = $('#NumberOfReportItemsToTake').val();
+    const viewer = $("#reportViewer").data("telerik_ReportViewer");
+
+    viewer.reportSource({
+        report: viewer.reportSource().report,
+        parameters: {
+            NumberOfReportItemsToTake: numberOfReportItemsToTake
+        }
+    });
+
+    //setting the HTML5 Viewer's reportSource causes a refresh automatically
+    //if you need to force a refresh for other cases, use:
+    //viewer.refreshReport();
+}
